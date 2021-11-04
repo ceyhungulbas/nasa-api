46a2c6ee7b7f44bddd3c25a169461273f16314bb
CeyhunGulbas <cetree61@gmail.com>
2021-11-05 00:26:45 +0300

    Fix: button image

--- src/components/MarsRoverPhotos/MRP.jsx
+++ src/components/MarsRoverPhotos/MRP.jsx
@@ -15,27 +15,15 @@ const MRP = () => {
     }, [])
 
     const randomPhotoFunc = () => {
-        setRandomPhoto(Math.floor(Math.random() * 856))
-        return roverPictures.photos[randomPhoto]
+        return setRandomPhoto(Math.floor(Math.random() * 856))
     }
-    // cok sacma olmadi ama duzeltilmesi lazim ama bunun icin cok sarhosum su an
-    // butona her tikladigin yeni pic gelecek falan iste
-    // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=FSEEknZd7Il94MUFY0U6ProdddlQzEHGLDeqOoLa
-    // https://api.nasa.gov/
     return(
         <>
             <h1>Photos from Mars Rover!</h1>
             <button onClick={randomPhotoFunc}>Generate!</button>
-            {/* <img src={roverPictures.photos.charAt(randomPhoto)} /> */}
+            <br />
+            <img src={roverPictures.photos[randomPhoto].img_src} alt={roverPictures.photos[randomPhoto].id} />
 
-            {/* {roverPictures.photos.map((pic) => {
-                
-                return(
-                    <div className="roverPicMapFuncDiv">
-                        <img src={pic.img_src} />
-                    </div>
-                )
-            })} */}
         </>
     )
 }
