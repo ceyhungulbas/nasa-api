b3021a534f2d676f54bf371fd18a375320c9dbc0
CeyhunGulbas <cetree61@gmail.com>
2021-11-04 23:33:17 +0300

    Fix: ONPROGRESS button image

--- src/components/MarsRoverPhotos/MRP.jsx
+++ src/components/MarsRoverPhotos/MRP.jsx
@@ -15,15 +15,18 @@ const MRP = () => {
     }, [])
 
     const randomPhotoFunc = () => {
-        return setRandomPhoto(Math.floor(Math.random() * 856))
+        setRandomPhoto(Math.floor(Math.random() * 856))
+        return roverPictures.photos[randomPhoto]
     }
     // cok sacma olmadi ama duzeltilmesi lazim ama bunun icin cok sarhosum su an
     // butona her tikladigin yeni pic gelecek falan iste
+    // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=FSEEknZd7Il94MUFY0U6ProdddlQzEHGLDeqOoLa
+    // https://api.nasa.gov/
     return(
         <>
             <h1>Photos from Mars Rover!</h1>
             <button onClick={randomPhotoFunc}>Generate!</button>
-            <img src={roverPictures.photos.charAt(randomPhoto)} />
+            {/* <img src={roverPictures.photos.charAt(randomPhoto)} /> */}
 
             {/* {roverPictures.photos.map((pic) => {
                 
