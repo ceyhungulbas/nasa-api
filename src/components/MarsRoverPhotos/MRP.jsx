e80c51b68ec473ef72c7908744be4d3fdd0fda4c
CeyhunGulbas <cetree61@gmail.com>
2021-11-13 15:23:10 +0300

    Fix: roverPictures undefined solve

--- src/components/MarsRoverPhotos/MRP.jsx
+++ src/components/MarsRoverPhotos/MRP.jsx
@@ -1,34 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const MRP = () => {
-    const [roverPictures, setRoverPictures] = useState([])
+    const [roverPictures, setRoverPictures] = useState()
     const [randomPhoto, setRandomPhoto] = useState(0)
 
-    // useEffect(() => {
-    //     fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=FSEEknZd7Il94MUFY0U6ProdddlQzEHGLDeqOoLa")
-    //         .then(res => res.json())
-    //         .then((data) => {
-    //             console.log("data: ", data);
-    //             console.log(data.photos.length);
-    //             setRoverPictures(data)
-    //         })
-    // }, [])
-
     useEffect(() => {
-        async function fetchRoverPhotos(){
-            
-        }
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=FSEEknZd7Il94MUFY0U6ProdddlQzEHGLDeqOoLa")
+            .then(res => res.json())
+            .then((data) => {
+                setRoverPictures(data)
+            })
     }, [])
 
     const randomPhotoFunc = () => {
-        return setRandomPhoto(Math.floor(Math.random() * 856))
+        return setRandomPhoto(Math.floor(Math.random() * 855))
     }
     return(
         <>
             <h1>Photos from Mars Rover!</h1>
             <button onClick={randomPhotoFunc}>Generate!</button>
             <br />
-            <img src={roverPictures.photos[randomPhoto].img_src} alt={roverPictures.photos[randomPhoto].id} />
+            <img src={roverPictures?.photos[randomPhoto].img_src} alt={roverPictures?.photos[randomPhoto].id} />
 
         </>
     )
